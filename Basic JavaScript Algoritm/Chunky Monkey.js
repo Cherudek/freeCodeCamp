7d9a9dcf59355ad2ca5de9e512e289a6bf1c8949
Gregorio Massara <greg.massara@gmail.com>
2017-01-24 22:42:57 +0000

    Add files via upload

---
+++ Basic JavaScript Algoritm/Chunky Monkey.js
@@ -0,0 +1,13 @@
+function chunkArrayInGroups(arr, size) {
+  
+var newArray = [];
+
+  
+while (arr.length) {
+newArray.push(arr.splice(0, size));
+}
+    // Break it up.
+  return newArray;
+}
+
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);
